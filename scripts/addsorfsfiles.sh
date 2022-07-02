2707013174a471680a033c976cd258c72a071982
Arun007coder <deepalali1007.agali@gmail.com>
2022-07-02 22:41:06 +0530

    Update Makefile

--- scripts/addsorfsfiles.sh
+++ scripts/addsorfsfiles.sh
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with SectorOS-RE2.  If not, see <http://www.gnu.org/licenses/>.
 
+make -f src/Makefile src/mksorfs
 
 for i in files/sorfs/*; do
     echo Adding "$i"
